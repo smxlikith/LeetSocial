a1b726677f664f83b30cff6e307eea76d488b13c
Likith Mohan <131691876+smxlikith@users.noreply.github.com>
2025-05-01 22:08:23 +0530

    fixed .env

--- be/config/db.js
+++ be/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+    path: "../.env.local.be"
+});
 
 export default async function () {
-        try {
-            const conn = await mongoose.connect(process.env.MONGO_URI);
-            console.log(`MongoDB Connected: ${conn.connection.host}`);
-        } catch (error) {
-            console.error(`Error: ${error.message}`);
-            process.exit(1);
-        }
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
 };
