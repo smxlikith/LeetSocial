725390ecf4b6ea73cc62321a077beb092c305801
Likith Mohan <131691876+smxlikith@users.noreply.github.com>
2025-05-01 21:48:13 +0530

    frontend v0

--- be/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { loginUser, refreshAccessToken } from '../controllers/authController.js';
-
-const router = express.Router();
-router.get("/callback", loginUser);
-router.get("/refresh-token", refreshAccessToken);
-
-export default router;
